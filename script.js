be2715af9432ceaee9eaa5a88fb6c22a98d3abd3
Chefsito22 <jordancamachox@gmail.com>
2022-01-28 16:33:09 -0500

    Add files

---
+++ script.js
@@ -0,0 +1,24 @@
+// El estudiante debe generar una salida igual a la de la imagen
+
+for (let i = 0 ; i <=9 ; i++) {
+    if (i==0 || i==9) {
+        console.log(`**********************`)
+    }else{console.log(`*                    *`)}
+}
+
+// Ejercicio TzStriker consiste en tomar N numeros y rrecorrelos con un iterador, en este ejercicio deben imprimir segun la condicion.
+
+let cantidad = 45
+
+for(let i = 1; i <= cantidad ; i++) {
+    if (i%15==0) {
+        console.log(`TzStrike`)
+    }else if (i%3==0) {
+        console.log(`Tz`)
+    }else if (i%5==0) {
+        console.log(`Strike`)
+    }else{
+        console.log(i)
+    }
+
+}
